f59d66d85977f05cebe12333bcad3981456dd375
nmnchu <namika1218@gmail.com>
2021-04-15 00:18:55 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,9 +35,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+
+    require "date"
+      days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    end
+
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
   end
-end
+
